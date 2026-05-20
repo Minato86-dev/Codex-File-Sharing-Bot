92aeb2a87f418280d5758bf8f5b8458bb38da0f8
Minato86-dev <79691253+Minato86-dev@users.noreply.github.com>
2026-05-20 13:04:43 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Mera Nirmaan Humari Anime Website TpXSub.com Ki Files Store Karne Ke Liye Kiya Gaya Hai, Mere <a href='tg://user?id={OWNER_ID}'>Nirmaata</a> hain.\n\nHumare Saath Bane Rehne Ke Liye Humare News Channel: @TeamProjectXNews Ko Join Karein, Aur Saath Hi Humare Group: @TpXNew Ko Bhi Join Karein!</b>",
+            text = f"<b>Mera Nirmaan Humari Anime Website TpXSub.com Ki Files Store Karne Ke Liye Kiya Gaya Hai, Mere Nirmaata <a href='tg://user?id={OWNER_ID}'>Minato-sama</a> Hain.\n\nHumare Saath Bane Rehne Ke Liye Humare News Channel: @TeamProjectXNews Ko Join Karein, Aur Saath Hi Humare Group: @TpXNew Ko Bhi Join Karein!</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
