fc492ba24aac79a1eef41edf5fa40f343c45087f
Minato86-dev <79691253+Minato86-dev@users.noreply.github.com>
2026-05-20 12:51:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text =f"<b>Mera Nirmaan Humari Anime Website TpXSub.com Ki Files Store Karne Ke Liye Kiya Gaya Hai, Mere <a href='tg://user?id={OWNER_ID}'>Nirmaata</a> hain.\n\nHumare Saath Bane Rehne Ke Liye Humare News Channel: @TeamProjectXNews Ko Join Karein, Aur Saath Hi Humare Group: @TpXNew Ko Bhi Join Karein!</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
